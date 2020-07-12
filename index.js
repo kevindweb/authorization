dea261183062ebe7df449ba53b2bd5fee1c81d2a
Kevin Deems <kevin8deems@gmail.com>
2020-07-12 19:08:55 +0000

    Create comment on failure

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const request = require("request")
 const core = require("@actions/core");
 const { context, GitHub } = require("@actions/github");
 
+function getSha() {
+    if (github.context.eventName == "pull_request") {
+        return github.context.payload.pull_request.head.sha;
+    } else {
+        return github.context.sha;
+    }
+}
+
 async function run() {
     const { USER } = process.env;
     if (!USER) {
@@ -11,23 +19,53 @@ async function run() {
             return;
     }
 
-    console.log("User is " + USER);
+    core.debug("user is: " + USER);
     const url = core.getInput("auth_url", { required: true });
 
     await request({
-            url: url,
-            json: true
+        url: url,
+        json: true
     }, function (error, response, body) {
-            if (!error && response.statusCode === 200 &&
-                    body.authorized_users.length > 0) {
-                    // response was valid, check user
-                    var present = body.authorized_users.includes(USER);
-                    core.setOutput("authorized", present.toString());
-            } else {
-                    console.log("Authentication URL returned no response");
-                    core.setOutput("authorized", "false");
+        if (!error && response.statusCode === 200 &&
+            body.authorized_users.length > 0) {
+            // response was valid, check user
+            if (body.authorized_users.includes(USER)) {
+                // authorized user
+                core.setOutput("authorized", 'true');
+                return;
             }
+
+            // unauthorized
+            unauthorized(USER + " is not authorized to run CI");
+        } else {
+            unauthorized("CI couldn't provide a list of authorized users");
+        }
     })
+}
+
+function unauthorized(message) {
+    const { GITHUB_TOKEN } = process.env;
+    const octokit = Github.getOctokit(GITHUB_TOKEN);
+    const sha = getSha();
+    const [owner, repo] = core.getInput("repository").split("/");
+
+    core.debug(repository);
+
+    var body = ">" + (context.eventName === "issue_comment"
+            ? context.payload.comment.body
+            : 'Response to PR creation') + "\n";
+
+    core.debug(body);
+
+    // create the comment on Github
+    octokit.repos.createCommitComment({
+      owner: owner,
+      repo: repo,
+      commit_sha: sha,
+      body: body + message,
+    });
+
+    core.setOutput("authorized", "false");
 }
 
 run().catch(err => {
